f510dbd1013a8f615be84526a83c6417318b5d0c
Nitaicandra <89361982+Nitaicandra@users.noreply.github.com>
2023-11-10 00:20:28 -0500

    some comments

--- snakegame/snake.py
+++ snakegame/snake.py
@@ -2,14 +2,18 @@ import pygame,sys
 import cell
 import grid
 
-
+# probably just a head and tail to snake cells # form a sort of linked list 
 class Snake:
     head=None #snake cell
     tail=None #snake cell
     
     def __init__(self):
         return
-
+        
+        
+    def move():
+        return
+    #either move takes in a string or enum paramater or we have 4 move functions for the cardinals
 
 
 
